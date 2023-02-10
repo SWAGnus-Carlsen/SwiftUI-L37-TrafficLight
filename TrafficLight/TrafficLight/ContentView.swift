ce83a47efb63dd82aab071af3653ada549c80135
SWAGnus-Carlsen <92265364+SWAGnus-Carlsen@users.noreply.github.com>
2023-02-10 16:26:55 +0300

    manual switching is done

--- TrafficLight/TrafficLight/ContentView.swift
+++ TrafficLight/TrafficLight/ContentView.swift
@@ -6,32 +6,48 @@
 //
 
 import SwiftUI
+enum TrafficLightState {
+    case redLight
+    case redAndYellowLight
+    case greenLight
+    case yellowLight
 
+}
 struct ContentView: View {
+    
+    @State var color1: CGColor?
+    @State var color2: CGColor?
+    @State var color3: CGColor?
+    @State var wasTappedAtLeastOnce = false
+    @State var state = TrafficLightState.yellowLight
+    var defaultColor = UIColor.systemGray.cgColor
     var body: some View {
         ZStack{
             Color(.black).ignoresSafeArea()
             VStack {
-                Color(.systemRed).frame(width: 150, height: 150)
+                Color(color1 ?? defaultColor).frame(width: 150, height: 150)
                     .overlay(Circle().stroke(Color .white, lineWidth:10))
                     .clipShape(Circle())
-                Color(.systemYellow)
+                Color(color2 ?? defaultColor)
                     .frame(width: 150, height: 150)
                     .overlay(Circle().stroke(Color .white, lineWidth:10))
                     .clipShape(Circle())
-                Color(.systemGreen)
+                Color(color3  ?? defaultColor)
                     .frame(width: 150, height: 150)
                     .overlay(Circle().stroke(Color .white, lineWidth:10))
                     .clipShape(Circle())
                 Spacer()
-                Button("START"){
-                    
-                }.frame(width: 200, height: 100)
-                    .overlay(Rectangle().stroke(Color .white, lineWidth:10).cornerRadius(5))
-                    .foregroundColor(Color.white)
-                    .background(Color.blue)
-                    .cornerRadius(50)
-                    
+                Button(action: { changeState()}) {
+                           HStack {
+                               Text(wasTappedAtLeastOnce ? "NEXT STATE" : "START")
+                                   .font(.custom("Seravek-Bold", size: 25))
+                                   .fontWeight(.bold)
+                                   .foregroundColor(Color.white)
+                           }
+                       }
+                     
+                       .buttonStyle(CustomButtonStyle())
+                       
                     
                 
                 
@@ -39,6 +55,38 @@ struct ContentView: View {
             .padding()
         }
         
+    }
+    func changeState() {
+        wasTappedAtLeastOnce = true
+        switch state {
+            
+        case .redLight:
+            state = .redAndYellowLight
+        case .redAndYellowLight:
+            state = .greenLight
+        case .greenLight:
+            state = .yellowLight
+        case .yellowLight:
+            state = .redLight
+        }
+        switch state {
+        case .redLight:
+            color1 = UIColor.red.cgColor
+            color2 = UIColor.systemGray.cgColor
+            color3 = UIColor.systemGray.cgColor
+        case .redAndYellowLight:
+            color1 = UIColor.red.cgColor
+            color2 = UIColor.systemYellow.cgColor
+            color3 = UIColor.systemGray.cgColor
+        case .greenLight:
+            color1 = UIColor.systemGray.cgColor
+            color2 = UIColor.systemGray.cgColor
+            color3 = UIColor.systemGreen.cgColor
+        case .yellowLight:
+            color1 = UIColor.systemGray.cgColor
+            color2 = UIColor.systemYellow.cgColor
+            color3 = UIColor.systemGray.cgColor
+        }
     }
 }
 
